3da9232d06a4fc4737fbd9b7bf522b1ca11c2908
Vo1z <vitya.voody@gmail.com>
2021-05-03 17:47:19 +0300

    modes-rework 1
    -Mode class was added
    -Destruction was reworked due Mode class
    -Placemet was reworked due Mode class
    -Movement was reworked due Mode class
    -Tile preview was added to placement

--- Assets/Scripts/Modes/Placement.cs
+++ Assets/Scripts/Modes/Placement.cs
@@ -1,21 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class Placement : MonoBehaviour
+public class Placement : Mode
 {
-    [SerializeField] private Player player;
-    [SerializeField] private GameObject blueprintToShow;
-    [SerializeField] private Vector3 raycastOffset;
-    private Tile _occupiedTile;
-    private List<Tile> _adjacentTiles = new List<Tile>();
-    
+    [Serializable]
+    private class TileBlueprintEntry
+    {
+        public GameObject blueprintPrefab;
+        public ETileType tileType;
+        public KeyCode keyThatSelectsTile;
+    }
+
+    [SerializeField] private TileBlueprintEntry[] blueprints;
+
+    private TileBlueprintEntry _selectedBlueprintEntry;
+    private GameObject _blueprintInstance;
+
+    private void Awake()
+    {
+        _selectedBlueprintEntry = blueprints[0];
+        _blueprintInstance = Instantiate(_selectedBlueprintEntry.blueprintPrefab);
+        _blueprintInstance.layer = LayerMask.NameToLayer("Ignore Raycast");
+        _blueprintInstance.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        _blueprintInstance.SetActive(false);
+    }
+
     private void Update()
     {
         GetAdjacentTiles();
 
         var mousePointRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-
+        
         if (Physics.Raycast(mousePointRay, out hit))
         {
             var selectedTile = hit.transform.GetComponent<Tile>();
@@ -23,11 +43,13 @@ public class Placement : MonoBehaviour
                 return;
 
             selectedTile = selectedTile.HighestTileFromAbove;
-
+            
             if (_adjacentTiles.Contains(selectedTile))
             {
+                DrawSelectedBlueprint(selectedTile);
+                
                 //todo replace hardcode
-                if (Input.GetMouseButtonDown(0) && player.HasSuchItemInInventory(ETileType.Floor))
+                if (Input.GetMouseButtonDown(0) && player.HasSuchItemInInventory(_selectedBlueprintEntry.tileType))
                 {
                     //todo remove hardcode
                     player.PlaceTile(ETileType.Floor, selectedTile);
@@ -36,98 +58,21 @@ public class Placement : MonoBehaviour
         }
     }
     
-    void GetAdjacentTiles()
+    private void DrawSelectedBlueprint(Tile baseTile)
     {
-        _adjacentTiles.Clear();
-        var startPosition = player.attachedTile.LowestTileFromUnderneath.transform.position;
-        for (int i = 0; i < 6; i++)
+        foreach (var blueprintEntry in blueprints)
         {
-            var ray = new Ray();
-            RaycastHit hit;
-            Vector3 direction = Vector3.zero;
-
-            switch (i)
-            { 
-                case 0:
-                    direction = new Vector3(1, 0, 0);
-                    break;
-                case 1:
-                    direction = new Vector3(1, 0, -1);
-                    break;
-                case 2:
-                    direction = new Vector3(-1,0, -1);
-                    break;
-                case 3:
-                    direction = new Vector3(-1,0, 0);
-                    break;
-                case 4:
-                    direction = new Vector3(-1,0, 1);
-                    break;
-                case 5:
-                    direction = new Vector3(1, 0, 1);
-                    break;
-                default:
-                    Debug.LogError("Failed to assign a direction");
-                    break;
-            }
-            
-            ray = new Ray(startPosition + raycastOffset, direction);
-
-            //todo reduce hardcode
-            if (Physics.Raycast(ray, out hit, 1.0f))
+            if (Input.GetKey(blueprintEntry.keyThatSelectsTile))
             {
-                var tile = hit.transform.GetComponent<Tile>();
-                if (tile != null)
-                {
-                    _adjacentTiles.Add(tile.HighestTileFromAbove);
-                    _adjacentTiles.Add(tile);
-                }
+                Destroy(_blueprintInstance);
+                _selectedBlueprintEntry = blueprintEntry;
+                _blueprintInstance = Instantiate(_selectedBlueprintEntry.blueprintPrefab);
+                _blueprintInstance.layer = LayerMask.NameToLayer("Ignore Raycast");
+                _blueprintInstance.SetActive(false);
             }
         }
-    }
-    private void OnDrawGizmos()
-    {
-        var rayDown = new Ray(player.transform.position, Vector3.down);
-        RaycastHit hit;
-        Tile attachedTile = null;
-        if (Physics.Raycast(rayDown, out hit))
-            attachedTile = hit.transform.GetComponent<Tile>();
-        if (attachedTile != null)
-            attachedTile = attachedTile.LowestTileFromUnderneath;
-            
         
-        if (!isActiveAndEnabled)
-            return;
-        for (int i = 0; i < 6; i++)
-        {
-            Vector3 direction = new Vector3();
-
-            switch (i)
-            {
-                case 0:
-                    direction = new Vector3(1, 0, 0);
-                    break;
-                case 1:
-                    direction = new Vector3(1, 0, -1);
-                    break;
-                case 2:
-                    direction = new Vector3(-1, 0, -1);
-                    break;
-                case 3:
-                    direction = new Vector3(-1, 0, 0);
-                    break;
-                case 4:
-                    direction = new Vector3(-1, 0, 1);
-                    break;
-                case 5:
-                    direction = new Vector3(1, 0, 1);
-                    break;
-                default:
-                    Debug.LogError("Failed to assign a direction");
-                    break;
-            }
-
-            Debug.DrawRay(attachedTile.transform.position + raycastOffset, direction);
-        }
+        _blueprintInstance.transform.position = baseTile.transform.position + baseTile.TileAbovePositionOffset;
+        _blueprintInstance.SetActive(true);
     }
 }
