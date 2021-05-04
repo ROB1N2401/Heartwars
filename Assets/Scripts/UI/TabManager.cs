1008bdd216a6aa9e317f5c69a4003a5d238eecaa
Ivan <iv_fomenko@bk.ru>
2021-05-04 12:28:24 +0200

    ui-alpha 1
    Initial commit:
    -implemented interactable buttons;
    -implemented tab controller script;
    -added placeholder assets

---
+++ Assets/Scripts/UI/TabManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabManager : MonoBehaviour
+{
+    private List<TabButton> _tabButtons = null;
+    private TabButton _selectedTab = null;
+
+    public TabButton SelectedTab { get => _selectedTab; set => _selectedTab = value; }
+
+    public void IncludeButton(TabButton button_in)
+    {
+        if (_tabButtons == null)
+        {
+            _tabButtons = new List<TabButton>();
+        }
+
+        _tabButtons.Add(button_in);
+    }
+}
