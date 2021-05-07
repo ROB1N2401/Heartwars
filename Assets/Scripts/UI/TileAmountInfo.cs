ef2762423278e59d229b37d2d3fd0c0ed41216a3
Ivan <iv_fomenko@bk.ru>
2021-05-07 14:23:21 +0200

    Dev 22
    -Imported New Input Master Package into the project
    -Imported free keys assets into the project
    -Imported Unity Samples for Input System Rebinding functionality
    -Remade Mode Selection script
    -Hardcoded visual representation of blocks left in the inventory
    -Hardcoded visual representation of Action Points left
    -Added visual hotkeys for each mode

---
+++ Assets/Scripts/UI/TileAmountInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TileAmountInfo : MonoBehaviour
+{
+    [SerializeField] private ETileType tileType;
+
+    private PlayerInventory _playerInventoryRef = null;
+    private TextMeshProUGUI _TMProRef = null;
+
+    void Awake()
+    {
+        _playerInventoryRef = FindObjectOfType<PlayerInventory>();
+        _TMProRef = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _TMProRef.text = _playerInventoryRef.GetNumberOfGivenTilesInInventory(tileType).ToString();
+    }
+}
