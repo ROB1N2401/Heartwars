e8459ffaa79e6f3200e9fe297ddbd4b0fe2bae2b
Yoel <yoelgrandevaos11@gmail.com>
2021-05-21 12:12:37 +0200

    Dev 49
    -Skybox+rotation
    -UI Assets
    -Hue change script

---
+++ Assets/Scripts/RotateSky.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour
+{
+    public float RotateSpeed = 1.2f;
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+    }
+}
