8cdfc4d1792743fbd56ea6df6776d9bad2b5969d
Vo1z <vitya.voody@gmail.com>
2021-05-31 00:23:06 +0200

    Dev 91
    -PP package was imported
    -PP was added to the main scene
    -Several scripts were modified (ActionPointsInfo, HueChange)
    -Particles were added to the camp tile
    -Rainbow effect was added to BonusTile

--- Assets/Scripts/Environment/SkyboxControl.cs
+++ Assets/Scripts/Environment/SkyboxControl.cs
@@ -6,8 +6,5 @@ public class SkyboxControl : MonoBehaviour
 {
     public float RotateSpeed = 1.2f;
 
-    void Update()
-    {
-        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
-    }
+    void Update() => RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
 }
--- Assets/Scripts/UI/ActionPointsInfo.cs
+++ Assets/Scripts/UI/ActionPointsInfo.cs
@@ -7,20 +7,7 @@ public class ActionPointsInfo : MonoBehaviour
 {
     private TextMeshProUGUI _TMProRef = null;
 
-    void Awake()
-    {
-        _TMProRef = GetComponent<TextMeshProUGUI>();
-    }
+    void Awake() => _TMProRef = GetComponent<TextMeshProUGUI>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        _TMProRef.text = PlayerManager.Instance.CurrentPlayer.PointsLeftForTheTurn.ToString();
-    }
+    void Update() => _TMProRef.text = $"{PlayerManager.Instance.CurrentPlayer.PointsLeftForTheTurn.ToString()} AP";
 }
